ec53e72ad0117c439d1dec698ad1dc598c686a49
Michael Victor <michael.victor@motusparts.co.za>
2022-03-31 18:35:14 +0200

    combined existing functions into working class

--- themanticAnalysis.py
+++ themanticAnalysis.py
@@ -1,17 +1,21 @@
 from collections import deque
 import re
+import glob
 
 """
 Tag description
 <id=001, tag=name, tone=P, comment=> stuff said in here </1>
 Tone = N negative, P = positive, N = Neutral
-    
+
+# Process:
+## Find all tags
+# -> Find index of all opening tags
+# -> FInd index of all corresponding closing tags
+# -> Append (opening index, Closing Index) to tags list    
     
 """
-
-
 class ThemanticAnalysis:
-    def __init__(self, dictOfFiles):
+    def __init__(self, fileList):
         """
         _summary_
 
@@ -21,84 +25,54 @@ class ThemanticAnalysis:
             {descriptive name: path to file}
         """
         
-        self.filePath = dictOfFiles
+        self.fileList = fileList # Path to files that must be analysed
+        self.interviewData = self.readFiles()
         
-    def readFile(filePath):    
     
-        return
+    def readFiles(self):
+                      
+        # Read all files, assign filename as key
+        files = {}
+        for file in self.fileList:
+                
+            # Extract filename
+            fileName = file.split('/')[-1][:-4]
+                
+            # Read file
+            with open(file, 'r') as fileObj:
+                fileContent = fileObj.readlines()
+                
+            #TODO Some filecleaning is reaquired here, strip ending spaces etc...
+            fileContentStr = ' '.join(fileContent)  
+                    
+            files[fileName] = fileContentStr 
+
+        self.interviewData = files   
     
     
-    def findTags(file):    
+    def findTagLocations(file):    
+            
+        # Find starts and ends of tags       
+        tagsStartIdx = {x.group()[4:7]: x.start() for x in re.finditer('<id....', file)}
+        tagsEndIdx = {x.group()[2:5]: x.start() for x in re.finditer('</...>', file)}
         
-        # Find starts and ends of tags
-        tagStartIdx = [{x.group()[4:7]:[x.start()]} for x in re.finditer('<id....', file)]     
-        tagEndIdx = [{x.group()[2:6]:[x.start()]} for x in re.finditer('</...>', file)]
         # Check that there are equal amount of open and close tags
+        test = len(tagsStartIdx) == len(tagsEndIdx)
+        assert (test), 'Number of opening tags must match number of closing tags' 
         
-        assert (len(tagStartIdx) != len(tagEndIdx)), 'Number of opening tags must match number of closing tags' 
+        tagsIdx = {key:(tagsStartIdx[key], tagsEndIdx[key]) for key, val in tagsStartIdx.items()} 
         
+        return tagsIdx
         
-        #for tag in tagsIdx:
-            
-            
-        
-        # Iterate though all tags to find matching parts using a stack
-        
-        return tagStartIdx, tagEndIdx
-        
-        
-# Process:
-## Find all tags
-# -> Find index of all opening tags
-# -> FInd index of all corresponding closing tags
-# -> Append (opening index, Closing Index) to tags list
-
-
-# Run a function to check that all tags are unique
-def findTagLocations(file):    
-        
-    # Find starts and ends of tags    
-    # tagsStartIdx = [{x.group()[4:7]:x.start()} for x in re.finditer('<id....', file)]     
-    tagsStartIdx = {x.group()[4:7]: x.start() for x in re.finditer('<id....', file)}
-    tagsEndIdx = {x.group()[2:5]: x.start() for x in re.finditer('</...>', file)}
-    
-    # Check that there are equal amount of open and close tags
-    test = len(tagsStartIdx) == len(tagsEndIdx)
-    assert (test), 'Number of opening tags must match number of closing tags' 
-    
-    # tagsIdx = {} 
-    # for key, _ in tagsStartIdx.items():
-    #      tagsIdx[key, (tagsStartIdx[key], tagsEndIdx[key])]
-     
-    tagsIdx = {key:(tagsStartIdx[key], tagsEndIdx[key]) for key, val in tagsStartIdx.items()} 
-     
-    return tagsIdx
-
-# def groupTags(tagStartIdx, tagEndIdx):
-    
-#     tagsIdx = 
-#     for key, val in tagStartIdx:
-        
-#     return tagsIdx
-
 
 if __name__ == '__main__': 
+    #C:\Users\michael.victor\Dropbox\Transcription
+    fileList = ['C:/Users/michael.victor/Dropbox/Transcription/transcription.txt']
+    
+    analysis = ThemanticAnalysis(fileList)
     
     
-    file = 'transcription.txt'
-    
-    with open(file, 'r') as fileObj:
-        file = fileObj.readlines()
-        
-    flatFile = ' '.join(file)
-    
-    # tagStartIdx = re.finditer('<id....', flatFile)    
-    # tagsIdx = [{x.group()[4:7]:[x.start()]} for x in tagStartIdx ]
-    
-    # Only excepts single string, not a list e.g, flat file
-    idx = findTagLocations(flatFile)
-        
-    
+
 # TODO: next steps in analysing links and relationships between interviewers and assigned tags        
 
 """
