1a40a1f7434454e36c8c6b71969b30bee2a40894
Michael Victor <michael.victor@motusparts.co.za>
2022-04-11 07:27:48 +0200

    change code to be more inline with how interface will work, simplify tags and add framwork

--- themanticAnalysis.py
+++ themanticAnalysis.py
@@ -5,6 +5,9 @@ import glob
 
 from collections import deque
 
+from yaml import full_load_all, SafeLoader
+
+
 """
 Tag description
 tag id is assigned automatically, in the background, just level needs to be specified
@@ -14,124 +17,71 @@ Tone = N negative, P = positive, N = Neutral
 
 """
 
+
 class Tag:
     """
     Data Class to store all tag information
     """
-    def __init__(self, startIdx, endIdx, rawText):
+    def __init__(self, tag, startIdx, endIdx, rawText):
+        self.tag = tag
         self.startIdx = startIdx
         self.endIdx = endIdx
-        self.rawText = rawText
-        self.tagList = self.extractTagList()
-        self.text = self.extractTagStr()
-
-        return
-
-    def extractTagList(self):
-        
-        try:        
-            tagList = re.search(
-                'tags\s*=\s*\\[(.*?)\s*\\]',
-                self.rawText,
-                re.IGNORECASE).group(1).split(',')
-        except:
-            raise Exception('No list of tags could be identified for tag')
-
-        return tagList
-
-    def extractTagStr(self):
-        
-        try:
-        
-            tagStrMatches = re.findall('>(.*?)</',
-                    self.rawText,
-                    re.IGNORECASE)
-            
-            # Find longest tag string
-            tagStr = max(tagStrMatches)           
-            return tagStr
-
-        except:
-            raise Exception('Cannot find tag string')
+        self.rawTextList = rawText
     
     def __repr__(self):
-        return f'<{", ".join(self.tagList)}>'
+        return f'<{self.tag}>'
     
 
 class Interview:
-    def __init__(self, name, text):
+    def __init__(self, file, name, text, framework, tags):
+        self.file = file
         self.name = name
-        self.rawText = text
-        self.tags = self.extractTags()
-        
-    def findRawTags(self):
-        openingTags = list(re.finditer('(<[^/][^<]+>)', self.rawText))
-        closingTags = list(re.finditer('(</..>)', self.rawText))
-        return openingTags, closingTags   
-        
-    def extractTags(self):
-        
-        openingTags, closingTags = self.findRawTags()
-        openingTags = deque(openingTags)
-                
-        # Find matching opening and closing tags
-        tags = []
-        while openingTags:
-            openTag = openingTags.popleft()
-            
-            tagId = re.search(
-                'id[" "]{0,10}=[" "]{0,10}(.*?)[" "]{0,10},',
-                openTag.group(0),
-                re.IGNORECASE) 
-
-            # If no Id is assigned, assume closing tag is next tag 
-            if tagId is None:
-                closeTag = closingTags.pop(0)
-                                  
-                tags.append(
-                    Tag(
-                        startIdx = openTag.end(),
-                        endIdx = closeTag.end(),
-                        rawText = self.rawText[openTag.start():closeTag.end()]
-                    )
-                ) 
-                 
-            # If tag id has been defined, find first closing tag with matching id
-            else:
-                for idx, closeTag in enumerate(closingTags):
-                    closeId = re.search(
-                        '<[" "]{0,10}/[" "]{0,10}(.*?)[" "]{0,10}>',
-                        openTag.group(0),
-                        re.IGNORECASE)
-                    
-                    if closeId == tagId:
-                        closeTag = closingTags.pop(idx)
-                    
-                tags.append(
-                    Tag(
-                        startIdx = openTag.start(),
-                        endIdx = closeTag.end(),
-                        rawText = self.rawText[openTag.Start():closeTag.end()]
-                    )
-                )  
-        return tags
+        self.text = text
+        self.framework = framework
+        self.rawText = self.readFile()
+        self.tags = tags
+  
+  
+    #TODO: function that creates codes from tags and framework 
     
-    def allTags(self):
-        allTags = {}
-        for tagObj in self.tags:                      
-            for tag in tagObj.tagList:
+    
+    def processTags(self, modelCodes, textCodes):
+        
+        for code in modelCodes:
+            
+            # Extract text codes
+            codeStrings = []
+            for item in textCodes:
+                if item['tag'] == code:
+                    codeStrings.append(item)
+
+            # Create code
+            testing = Tag(
+                description = modelCodes['definition'],
+                definition = modelCodes[''],
+                startIds = 1,
+                endIdx = 1,
+            )
+    
+        
+        return 
+    
+    # def allTags(self):
+    #     allTags = {}
+    #     for tagObj in self.tags:                      
+    #         for tag in tagObj.tagList:
                 
-                #breakpoint()
+    #             #breakpoint()
                 
-                if tag in allTags.keys():
-                    allTags[tag].append(tagObj.text)
-                else:
-                    allTags[tag] = [tagObj.text]
-        return allTags
+    #             if tag in allTags.keys():
+    #                 allTags[tag].append(tagObj.text)
+    #             else:
+    #                 allTags[tag] = [tagObj.text]
+    #     return allTags
     
     
 class ThemanticAnalysis:
-    def __init__(self, fileList):
+    def __init__(self, projectLocation):
         """
         
         process
@@ -146,56 +96,124 @@ class ThemanticAnalysis:
             {descriptive name: path to file}
         """
         
-        self.fileList = fileList # Path to files that must be analysed
-        self.interviews = self.readFiles()        
-               
-               
-    # def processInterviewData(self):
+        self.projectLocation = projectLocation
+        self.interviews = []        
+
+        self.readData()  
+                 
     
-    #TODO: convienience function that runs entire analysis
+    def readData(self):
         
-    #     files = self.readFiles()
-        
-    #     tagsIdx = self.findTagLocations(self)
-        
-        
-    #     findTags(file)
-        
-    #     return interviewdata        
+        projectData = self.readYaml()
     
-    def readFiles(self):
-                      
-        # Read all files, assign filename as key
-        interviews = []
-        for file in self.fileList:
-                
-            # Extract filename
-            fileName = file.split('/')[-1][:-4]
-                
-            # Read file
-            with open(file, 'r') as fileObj:
-                fileContent = fileObj.readlines()
-                
-            fileContentStr = ' '.join(fileContent)  
+        interviewList = []
+        for interview in projectData:
             
-            interviews.append(
+            # Get interview Text
+            file = f'{self.projectLocation}/interviews/{interview["file"]}'
+            interviewText = self.readInterview(file)
+
+            # Extract interview Codes            
+            openingTags, closingTags =  self.findRawTags(
+                interviewText = interviewText,
+                openingTagFilterStr='<{1}[^/<]+>{1}',
+                closingTagFilterStr='</{1}[^<>]+>{1}'
+            )   
+            tags = self.getTags(openingTags, closingTags, interviewText)                
+                        
+            interviewList.append(
                 Interview(
-                  name = fileName,
-                  text = fileContentStr  
+                    file = file,
+                    name = interview['interview'],
+                    text = interviewText,
+                    frameWork = interview['code framework'], 
+                    tags = tags                  
                 )
             )
+        return interviewList
 
-        return interviews
-    
+    def findRawTags(self, openingTagFilterStr, closingTagFilterStr, interviewText):
+        openingTags = list(re.finditer(openingTagFilterStr, interviewText))
+        closingTags = list(re.finditer(closingTagFilterStr, interviewText))
         
+        if len(openingTags) == len(closingTags):                
+            return openingTags, closingTags
+        else:
+            raise Exception('Amount of opening tags should equal amount of closing tags')  
+        
+    def getTags(self, openingTags, closingTags, interviewText):
+        
+        openingTags = deque(openingTags)
+        
+        # Find matching opening and closing tags
+        tags = []
+        
+        while openingTags:
+            openTag = openingTags.popleft()
+            
+            tagId = re.search(
+                'id[" "]{0,10}=[" "]{0,10}(.*?)[" "]{0,10},',
+                openTag.group(0),
+                re.IGNORECASE) 
+
+            # If no Id is assigned, assume closing tag is next tag 
+            if tagId is None:
+                closeTag = closingTags.pop(0)                   
+                tags.append(
+                    Tag(
+                        tag = openTag.group(0)[1:-1],
+                        startIdx = openTag.end(),
+                        endIdx = closeTag.end(),
+                        rawText = interviewText[openTag.start():closeTag.end()]
+                    )
+                )
+
+            # If tag id has been defined, find first closing tag with matching id
+            else:
+                for idx, closeTag in enumerate(closingTags):
+                    closeId = re.search(
+                        '<[" "]{0,10}/[" "]{0,10}(.*?)[" "]{0,10}>',
+                        openTag.group(0),
+                        re.IGNORECASE)
+                    
+                    if closeId == tagId:
+                        closeTag = closingTags.pop(idx)
+                    
+                tags.append(
+                    Tag(
+                        tag = openTag.group(0)[1:-1],
+                        startIdx = openTag.end(),
+                        endIdx = closeTag.end(),
+                        rawText = interviewText[openTag.start():closeTag.end()]
+                    )
+                )
+
+        return tags # [{'tag': {description: 'code definition', 'tagged text': text that has been tagged}}]    
+        
+    def readInterview(self, file):
+        with open(file, 'r') as fileObj:
+            fileContent = fileObj.readlines()
+        fileContentStr = ' '.join(fileContent)
+        return fileContentStr  
+        
+    def readYaml(self):
+        # Select first yaml file in project folder
+        projectYaml = glob.glob(self.projectLocation + '/' + '*.yml')[0]
+        
+        with open(projectYaml) as f:
+            file = full_load_all(f)
+            projectData = []
+            for doc in file:
+                projectData.append(doc)
+        return projectData
+
+#==============================================================================#       
 
 if __name__ == '__main__': 
-    fileList = [
-        'C:/Users/michael.victor/Dropbox/Transcription/test_interview.txt',
-        'C:/Users/michael.victor/Dropbox/Transcription/test_interview_2.txt'
-        ]
     
-    analysis = ThemanticAnalysis(fileList)
+    projectLocation = 'C:/Users/michael.victor/Dropbox/interview project'
+
+    analysis = ThemanticAnalysis(projectLocation)
     
     
     
