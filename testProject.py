2e6f4886105347691d387e09b8a3ea991edee653
Michael Victor <michaeltv.sa@gmail.com>
2022-04-21 06:53:14 +0200

    add test project to repo

---
+++ testProject.py
@@ -0,0 +1,24 @@
+    
+
+if __name__ == '__main__': 
+    
+    projectLocation = 'C:/Users/michael.victor/Dropbox/interview project'
+
+    analysis = ThemanticAnalysis(projectLocation)
+    
+    
+    
+    
+
+# TODO: next steps in analysing links and relationships between interviewers and assigned tags        
+
+"""
+see following links for data analysis
+https://datavizcatalogue.com/index.html_
+https://datavizcatalogue.com/methods/wordcloud.html
+https://datavizcatalogue.com/methods/parallel_coordinates.html
+https://datavizcatalogue.com/methods/radar_chart.html
+
+check relationships between team size, experience, industry, etc
+
+"""
