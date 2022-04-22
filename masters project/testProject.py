1f54f64942eb18e75516028fe35439282a066e48
Michael Victor <michaeltv.sa@gmail.com>
2022-04-22 09:27:53 +0200

    Add demo project script

---
+++ masters project/testProject.py
@@ -0,0 +1,23 @@
+
+from src.core.themanticAnalysis import ThemanticAnalysis
+    
+projectLocation = 'C:/Users/michael.victor/Dropbox/masters project'
+
+analysis = ThemanticAnalysis(projectLocation)
+
+
+
+
+
+# TODO: next steps in analysing links and relationships between interviewers and assigned tags        
+
+"""
+see following links for data analysis
+https://datavizcatalogue.com/index.html_
+https://datavizcatalogue.com/methods/wordcloud.html
+https://datavizcatalogue.com/methods/parallel_coordinates.html
+https://datavizcatalogue.com/methods/radar_chart.html
+
+check relationships between team size, experience, industry, etc
+
+"""
